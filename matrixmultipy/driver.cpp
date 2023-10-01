26f93e5216934f8c83212f8010b36541d1a4b5e4
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-01 09:32:05 -0700

    good control flow

--- matrixmultipy/driver.cpp
+++ matrixmultipy/driver.cpp
@@ -7,25 +7,27 @@ void matrixmult(vector<matrixrow *> A, vector<matrixrow *> B) {
 
   return;
 }
+bool isPowerOfTwo(int n) {
+  if (n == 0)
+    return false;
 
+  return (ceil(log2(n)) == floor(log2(n)));
+}
+void padB(vector<matrixrow *> *&B) {
+  // pad B with zeroes until size is power of two
+}
+void padA(vector<matrixrow *> *&A) {
+  // pad B with zeroes until size is power of two
+}
 void padmatrix(vector<matrixrow *> *A, vector<matrixrow *> *B) {
-  bool Agood = (A->size() % 2 != 0);
+  // express
+  bool Agood = (isPowerOfTwo(A->size()));
   if (Agood) {
     cout << "A NOT PADDED" << endl;
   } else {
     cout << "B NOT PADDED" << endl;
   }
-  switch (Agood) {
-  case 0: {
-    // B is culprit;
-
-  }
-
-  case 1: {
-    // A is culprit for pad with zeroes
-
-  }
-  }
+  Agood ? padB(B) : padA(A);
 }
 
 int main(int argc, char **argv) {
@@ -35,9 +37,9 @@ int main(int argc, char **argv) {
     // instead of crashing for non-square 2n matrices.
     vector<matrixrow *> *A = p->filein(argv[1]);
     vector<matrixrow *> *B = p->filein(argv[2]);
-    bool safe = (A->size() % 2 == 0 && B->size() % 2 == 0);
-    safe ? matrixmult(*A, *B) : padmatrix(A, B);
-
+    bool safe = (isPowerOfTwo(A->size()) && isPowerOfTwo(B->size()));
+    safe ? donothing() : padmatrix(A, B);
+    matrixmult(*A, *B);
     A->clear();
     B->clear();
     A = B = 0x0;
