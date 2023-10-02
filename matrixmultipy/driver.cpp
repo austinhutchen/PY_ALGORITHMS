3c10668f2b064159c1955b00f0134bcbf116bffc
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-10-02 08:13:23 -0700

    good pad

--- matrixmultipy/driver.cpp
+++ matrixmultipy/driver.cpp
@@ -15,12 +15,10 @@ bool isPowerOfTwo(int n) {
 }
 void pad(vector<matrixrow *> *&nums) {
   // pad with zeroes until size is power of two
-  vector<matrixrow *> pad;
-  matrixrow *padrow = new matrixrow(nums[0].size(), 0);
-  pad.push_back(padrow);
+  nums->push_back(new matrixrow(nums[0].size(), 0));
 }
 
-void padmatrix(vector<matrixrow *> *A, vector<matrixrow *> *B) {
+void padmatrix(vector<matrixrow *> *&A, vector<matrixrow *> *&B) {
   (isPowerOfTwo(A->size())) ? donothing() : pad(A);
   (isPowerOfTwo(B->size())) ? donothing() : pad(B);
 }
@@ -34,6 +32,8 @@ int main(int argc, char **argv) {
     vector<matrixrow *> *B = p->filein(argv[2]);
     vector<matrixrow *>::iterator a = A->begin();
     vector<matrixrow *>::iterator b = B->begin();
+    bool safe = (isPowerOfTwo(A->size()) && isPowerOfTwo(B->size()));
+    safe ? donothing() : padmatrix(A, B);
     cout << "A DIMENSION: " << A->size() << " "
          << "B DIMENSION: " << B->size() << endl;
     cout << "MATRIX A:" << endl;
@@ -46,8 +46,6 @@ int main(int argc, char **argv) {
       (*b)->print();
       b++;
     }
-    bool safe = (isPowerOfTwo(A->size()) && isPowerOfTwo(B->size()));
-    safe ? donothing() : padmatrix(A, B);
     matrixmult(*A, *B);
     A->clear();
     B->clear();
