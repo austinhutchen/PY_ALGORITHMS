202e0148187963c41aa67840c8ba066c1298ac59
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-07 10:47:40 -0800

    d

--- LNGST_CMMN_PREFIX.cpp
+++ LNGST_CMMN_PREFIX.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    void ptrswap(vector<string>::iterator str1, vector<string>::iterator  str2){
+    void ptrswap(vector<string>::iterator &str1, vector<string>::iterator  &str2){
     string temp = *str1;
     *str1 = *str2;
     *str2 = temp;
@@ -16,7 +16,7 @@ public:
         // use quicksort
    for(;p!=strs.end();++p){
      for(vector<string>::iterator x=std::next(p,1);x!=strs.end() ;++x){
-         if (*x <= *p){
+         if (*x < *p){
           ptrswap(x,p);
          }
      }
@@ -24,11 +24,11 @@ public:
    
    int i=0;
    string ans="";
-   (strs[strs.size()-1].compare("")==0)?i=1:i=0;
+   (strs[strs.size()-1]==ans)?i=1:i=0;
    if(i==1){
        return ans;
    }
-    while(strs[0][i]==strs[strs.size()-1][i]&& i<strs.size()){
+    while(strs[0][i]==strs[strs.size()-1][i]&& i<strs[strs.size()-1].size()){
            ans+=strs[0][i];
    i++;
     }
