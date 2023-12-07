f4124a41476e3971f3aacf8f29388dc2da9fb24a
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-07 10:34:31 -0800

    d

--- LNGST_CMMN_PREFIX.cpp
+++ LNGST_CMMN_PREFIX.cpp
@@ -1,9 +1,3 @@
-#include <iostream>
-using namespace std;
-
-
-
-
 class Solution {
 public:
 
@@ -13,19 +7,28 @@ public:
     *str2 = temp;
     return;
     }
+
     string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==1){
+            return strs[0]; 
+        }
         vector<string>::iterator p = strs.begin();
         // use quicksort
    for(;p!=strs.end();++p){
-     for(vector<string>::iterator x=p;x!=strs.end();++x){
-         if (*x < *p){
+     for(vector<string>::iterator x=std::next(p,1);x!=strs.end() ;++x){
+         if (*x <= *p){
           ptrswap(x,p);
          }
      }
    }
+   
    int i=0;
    string ans="";
-    while(strs[0][i]==strs[strs.size()-1][i]){
+   (strs[strs.size()-1].compare("")==0)?i=1:i=0;
+   if(i==1){
+       return ans;
+   }
+    while(strs[0][i]==strs[strs.size()-1][i]&& i<strs.size()){
            ans+=strs[0][i];
    i++;
     }
