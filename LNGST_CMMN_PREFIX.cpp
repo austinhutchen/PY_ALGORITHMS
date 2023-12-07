bbaf93c6e077dee186459ba72e5a3ac4f2d6cdc5
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-07 12:18:12 -0800

    d

--- LNGST_CMMN_PREFIX.cpp
+++ LNGST_CMMN_PREFIX.cpp
@@ -35,3 +35,45 @@ public:
        return ans;
 }
 };
+
+
+
+
+
+
+
+// str2
+//
+// LNGST_CMMN_PREF
+// class Solution {
+public:
+
+    void ptrswap(vector<string>::iterator &str1, vector<string>::iterator  &str2){
+    string temp = *str1;
+    *str1 = *str2;
+    *str2 = temp;
+    return;
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==1){
+            return strs[0]; 
+        }
+        vector<string>::iterator p = strs.begin();
+        // use quicksort
+   for(;p!=strs.end();++p){
+     for(vector<string>::iterator x=std::next(p,1);x!=strs.end() ;++x){
+         if (*x < *p){
+          ptrswap(x,p);
+         }
+     }
+   }  char *cp = &strs[0][0]; const char* endp1=&strs[0][strs[0].size()-1];
+    char*cp2 = &strs[strs.size()-1][0]; const char* endp2= &strs[strs.size()-1][strs[strs.size()-1].size()-1];
+   string ans="";
+    while(*cp==*cp2&& cp != endp1 && cp2!=endp2){
+           ans+= *cp;
+   ++cp; ++cp2;
+    }
+       return ans;
+}
+};
